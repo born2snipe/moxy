7ace71d807252fb9991182481fec9f191c7f907a
Dan Dudley <born2snipe@gmail.com>
2016-09-14 21:37:30 -0500

    added some javadoc info

--- src/main/java/moxy/MoxyServer.java
+++ src/main/java/moxy/MoxyServer.java
@@ -35,6 +35,12 @@ public class MoxyServer {
     private Map<Integer, ConnectTo> listenOnPortToRemote = Collections.synchronizedMap(new LinkedHashMap<>());
     private DispatchListener dispatchListener = new DispatchListener();
 
+    /**
+     * Provide what local port you would like to listen on
+     *
+     * @param portToListenOn - the local port number to bind to and begin listening for incoming connections
+     * @return the instance of the RouteTo to tell where to route the traffic
+     */
     public RouteTo listenOn(int portToListenOn) {
         return new RouteTo() {
             public void andConnectTo(InetSocketAddress socketAddress) {
@@ -50,12 +56,9 @@ public class MoxyServer {
         };
     }
 
-    private void assertPortIsNotAlreadySetup(int portToListenOn) {
-        if (listenOnPortToRemote.containsKey(portToListenOn)) {
-            throw new IllegalArgumentException("There can only be one route for a single port number. It appears port number [" + portToListenOn + "] is already setup.");
-        }
-    }
-
+    /**
+     * Tell the server to bind and start listening for incoming connections
+     */
     public void start() {
         if (started.get()) {
             log.warn("Server already started");
@@ -71,6 +74,64 @@ public class MoxyServer {
         } catch (RuntimeException e) {
             stop();
             throw e;
+        }
+    }
+
+    public void setLog(Log log) {
+        this.log = log;
+    }
+
+    /**
+     * Tell the server to disconnect any connections and stop listening for any new connections
+     */
+    public void stop() {
+        log.info("Stopping all port listeners...");
+        listenOnPortToRemote.values().forEach(ConnectTo::shutdown);
+        started.set(false);
+    }
+
+    /**
+     * Tell the server to stop listening on the provided port, but do not remove the routing configuration
+     * <p>
+     * Note: this is very similar to removeListenerOn(...), but the route configuration will live through a server restart
+     *
+     * @param portNumber - the local port you would like to stop listening for incoming connections
+     */
+    public void stopListeningOn(int portNumber) {
+        log.info("Stop listening on port: " + portNumber);
+        ConnectTo connectTo = listenOnPortToRemote.get(portNumber);
+        if (connectTo != null) {
+            connectTo.shutdown();
+        } else {
+            log.warn("Nothing is listening on port [" + portNumber + "]");
+        }
+    }
+
+    /**
+     * Tell the server to stop listening on the provided port and remove the routing information also
+     * <p>
+     * Note: this is very similar to stopListeningOn(...), but the route configuration will NOT be available if you choose to restart the server
+     *
+     * @param portNumber - the local port you would like to stop listening for incoming connections
+     */
+    public void removeListenerOn(int portNumber) {
+        log.info("Removing listener on port: " + portNumber);
+        stopListeningOn(portNumber);
+        listenOnPortToRemote.remove(portNumber);
+    }
+
+    /**
+     * Add a listener to get notified when certain events happen
+     *
+     * @param listener - the instance of the listener to be registered
+     */
+    public void addListener(MoxyListener listener) {
+        dispatchListener.addListener(listener);
+    }
+
+    private void assertPortIsNotAlreadySetup(int portToListenOn) {
+        if (listenOnPortToRemote.containsKey(portToListenOn)) {
+            throw new IllegalArgumentException("There can only be one route for a single port number. It appears port number [" + portToListenOn + "] is already setup.");
         }
     }
 
@@ -114,36 +175,6 @@ public class MoxyServer {
         }
 
         exceptionHolder.reThrowAsNeeded();
-    }
-
-    public void setLog(Log log) {
-        this.log = log;
-    }
-
-    public void stop() {
-        log.info("Stopping all port listeners...");
-        listenOnPortToRemote.values().forEach(ConnectTo::shutdown);
-        started.set(false);
-    }
-
-    public void stopListeningOn(int portNumber) {
-        log.info("Stop listening on port: " + portNumber);
-        ConnectTo connectTo = listenOnPortToRemote.get(portNumber);
-        if (connectTo != null) {
-            connectTo.shutdown();
-        } else {
-            log.warn("Nothing is listening on port [" + portNumber + "]");
-        }
-    }
-
-    public void removeListenerOn(int portNumber) {
-        log.info("Removing listener on port: " + portNumber);
-        stopListeningOn(portNumber);
-        listenOnPortToRemote.remove(portNumber);
-    }
-
-    public void addListener(MoxyListener listener) {
-        dispatchListener.addListener(listener);
     }
 
     public interface RouteTo {
